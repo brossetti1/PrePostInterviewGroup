162b9d011953b27a75b5e6351cb83c0c30d9b124
brossetti1 <brossetti1@gmail>
2015-03-07 18:16:14 -0500

    fixed application contorller and authenticaiton from request headers. create for jobs is working now, jbuilder is up for views.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
-  before_action :set_company, [:create]
+  before_action :authentication_user_from_token!
+  before_action :set_company, only: [:create, :index]
   before_action :set_job, only: [:update, :destroy, :show]
 
   def index
@@ -9,9 +10,10 @@ class JobsController < ApplicationController
   end
 
   def create
-    binding.pry
-    @job = Job.new(job_params, company_id: @co.id, user_id: @cu.id)
-    if @job.save
+    @job = Job.new(job_params)
+    @job.company_id = @co.id; @job.user_id = current_user.id 
+    @job.save; make_pros(@job.id); make_cons(@job.id)
+    if @job.id?
       render "jobs/create.json.jbuilder", status: :created
     else
       render json: { error: @job.errors.full_messages }, status: :unprocessable_entity
@@ -22,6 +24,7 @@ class JobsController < ApplicationController
   end
 
   def update
+    @job_cp = @job.dup
     #TODO check error if errors or if no changes???
     @job.update_attributes(job_params)
     excpt_attr = 'id','created_at','updated_at'
@@ -33,7 +36,7 @@ class JobsController < ApplicationController
   end
 
   def destroy
-
+    @job.destroy
   end
 
   private
@@ -43,17 +46,28 @@ class JobsController < ApplicationController
 
   def set_job
     @job = Job.find(params[:id])
-    @job_cp = @job.dup
-  end
-
-  def current_user_ref
-    @cu = current_user
   end
 
   def job_params
-    bidning.pry
-    params.require(:job).permit(:job_title, :lead_source, :pro, :con,
-                                :short_summary, :salary)
+    params.require(:job).permit(:job_title, :lead_source, :short_summary, :salary)
+  end
+
+  def make_pros(job_id)
+    ary = params[:pro]['pros'] || nil
+    if ary
+      ary.each do |text|
+        Pro.create(pro:text, job_id: job_id)
+      end
+    end
+  end
+
+  def make_cons(job_id)
+    ary = params[:con]['cons'] || nil
+    if ary
+      ary.each do |text|
+        Con.create(con:text, job_id: job_id)
+      end
+    end
   end
 
 end
@@ -62,15 +76,23 @@ end
 
 #test data:
 
-#create
+#create job
 # {
-#   "job": 
+#  "job": 
 #     {
 #       "job_title": "testingstuff", 
 #       "lead_source": "testingstuff", 
-#       "pro": "[testingstuff,test,testt]", 
-#       "con": "[testingstuff,test,testt]",
 #       "short_summary": "testingstuff", 
 #       "salary": 1425352
+#     },
+    
+#  "pro": 
+#     {
+#       "pro": ["testing pro create","testprocreate","test_pro_create"]
+#     },
+    
+#   "con":
+#     {
+#       "con": ["testing pro create","testprocreate","test_pro_create"]
 #     }
 # }
--- app/views/jobs/create.json.jbuilder
+++ app/views/jobs/create.json.jbuilder
@@ -1,12 +1,18 @@
 json.job do
   json.job_title @job.job_title
   json.lead_source @job.lead_source
-  json.pro @job.pro
-  json.con @job.con
   json.short_summary @job.short_summary
-  json.salary @job.salary @
+  json.salary @job.salary
   json.company_id @job.company_id
   json.created_at @job.created_at
   json.updated_at @job.updated_at
   json.user_id @job.user_id
+end
+
+json.cons do 
+  json.cons @job.cons, :con
+end
+
+json.pros do 
+  json.pros @job.pros
 end
